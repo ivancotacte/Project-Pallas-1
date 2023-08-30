f1b38fdbe3da78d8f6eaff225f0da48ca2a7eedb
Ivan Cotacte <cotactearmenion@gmail.com>
2023-08-30 19:55:06 +0800

    second

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,8 +1,40 @@
-module.exports = ({ api, event, config, userInfo }) => {
-  if (event.logMessageType == "log:subscribe") {
-    require("../events/join.js")({ api, event, config });
-  } else if (event.logMessageType == "log:unsubscribe") {
-    require("../events/left.js")({ api, event, config });
-  } else {
+const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+const https = require("https");
+
+const config = require("../config");
+const { banned } = config;
+
+module.exports = ({ api, event }) => {
+  try {
+    if (event.logMessageType == "log:subscribe") {
+      const addedParticipants = event.logMessageData.addedParticipants;
+      addedParticipants.forEach(async (participant) => {
+        const joinMemberID = participant.userFbId;
+        const joinMemberInfo = await api.getUserInfo(joinMemberID);
+        const joinMemberfullName = joinMemberInfo[joinMemberID].name;
+        const welcome = [`Wassup,`, `Welcome,`, "Supp,", "Wazzup,"];
+        const randomIndex = Math.floor(Math.random() * welcome.length);
+        const welcomeMessage = welcome[randomIndex];
+        var imageUrl = `https://api-test.tapikej101.repl.co/api/fbimage/${joinMemberID}`;
+        var imagePath = path.join(__dirname, `/../cache/${joinMemberID}.jpg`);
+        const imageStream = fs.createWriteStream(imagePath);
+        https.get(imageUrl, (response) => {
+          response.pipe(imageStream);
+          imageStream.on("finish", () => {
+            api.sendMessage(
+              {
+                body: `${welcomeMessage} @${joinMemberfullName}`,
+                attachment: fs.createReadStream(imagePath),
+              },
+              event.threadID,
+            );
+          });
+        });
+      });
+    }
+  } catch (error) {
+    console.error("Error:", error);
   }
 };
--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 const { Listen } = require("./login");
-const config = require("./config");
 const fs = require("fs");
+
+
+const config = require("./config");
+const { admins, banned } = config;
+
 
 Listen(async (api, event) => {
   let userInfo = await api.getUserInfo(event.senderID);
   userInfo = userInfo[event.senderID];
 
   if (event.type == "message_reply" || event.type == "message") {
+    try {
+      if (input.startsWith("!shell")) {
+        if (admins.includes(event.senderID)) {
+          let data = event.body.split(" ");
+          const { exec } = require("child_process");
+          if (data.length < 2) {
+            api.sendMessage("Enter command.", event.threadID, event.messageID);
+          } else {
+            data.shift();
+            let cmd = data.join(" ");
+            exec(cmd, (error, stdout, stderr) => {
+              if (error) {
+                api.sendMessage(
+                  `Error: \n${error.message}`,
+                  event.threadID,
+                  event.messageID,
+                );
+                return;
+              }
+              if (stderr) {
+                api.sendMessage(
+                  `Stderr:\n ${stderr}\n${stdout}`,
+                  event.threadID,
+                  event.messageID,
+                );
+                return;
+              }
+              api.sendMessage(`${stdout}`, event.threadID, event.messageID);
+            });
+          }
+        } else {
+          api.sendMessage(
+            "Sorry, but you do not have the necessary permission to use this command.",
+            event.threadID,
+            event.messageID,
+          );
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   } else if (event.type == "event") {
-    require("./handlers/eventHandler")({ api, event, config, userInfo });
+    require("./handlers/eventHandler")({ api, event });
   }
 });
